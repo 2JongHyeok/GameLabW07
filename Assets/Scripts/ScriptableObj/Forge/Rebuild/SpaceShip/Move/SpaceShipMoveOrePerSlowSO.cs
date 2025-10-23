e1d9045867965379f6492b2e10b53a9a7551a950
HongjunJo <hongjunjo23@gmail.com>
2025-10-23 15:37:24 +0900

    광물 운반량 수정

--- Assets/Scripts/ScriptableObj/Forge/Rebuild/SpaceShip/Move/SpaceShipMoveOrePerSlowSO.cs
+++ Assets/Scripts/ScriptableObj/Forge/Rebuild/SpaceShip/Move/SpaceShipMoveOrePerSlowSO.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SpaceShipMoveOrePerSlowSO", menuName = "ScriptableObjects/Forge/SpaceShip/Move/SpaceShipMoveOrePerSlowSO", order = 1)]
 public class SpaceShipMoveOrePerSlowSO : BaseForgeSO
 {
-    public int OrePerSlow;
+    public int allowedOreCount;
     
     protected override ForgeId GetForgeId() => ForgeId.SpaceShipMoveOrePerSlow;
     
     public override void Apply()
     {
         if (Managers.Instance?.spaceshipMotor == null) return;
-        Managers.Instance.spaceshipMotor.AddAllowedOreCount(OrePerSlow);
+        Managers.Instance.spaceshipMotor.AddAllowedOreCount(allowedOreCount);
     }
 }
